2a6d39ba6e2617165a9a6190a82744365b239125
axel669 <haryuu.hanekata@gmail.com>
2019-06-03 12:29:47 -0700

    started tests for escape sequence

---
+++ tests/test-escape.js
@@ -0,0 +1,20 @@
+const update = require("../index.js")
+
+const source = require("./source.js")
+
+describe("escaped", () => {
+    describe("$set", () => {
+        it("top level", () => {
+            const updated = update(
+                source,
+                {"top..test.$set": 100}
+            )
+
+            expect(updated)
+                .toEqual({
+                    "top.test": 100,
+                    ...source
+                })
+        })
+    })
+})
