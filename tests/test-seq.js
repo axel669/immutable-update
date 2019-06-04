19a3e1597fd09e4bce606846e540d556249bce72
axel669 <haryuu.hanekata@gmail.com>
2019-06-04 14:39:22 -0700

    tests for update.seq

---
+++ tests/test-seq.js
@@ -0,0 +1,16 @@
+const update = require("../index.js")
+
+describe("seq", () => {
+    it("keeps order", () => {
+        const updated = update.seq(
+            {a: 1, b: 2},
+            ["a.$apply", a => a + 3],
+            ["a.$apply", a => a / 2],
+            ["b.$apply", b => b + 7],
+            ["b.$apply", b => b / 3],
+        )
+
+        expect(updated)
+            .toEqual({a: 2, b: 3})
+    })
+})
