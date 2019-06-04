82828bb7a7676688e6e0db477494f89abcd12712
axel669 <haryuu.hanekata@gmail.com>
2019-06-04 14:14:32 -0700

    tests for $apply

---
+++ tests/test-$apply.js
@@ -0,0 +1,43 @@
+const update = require("../index.js")
+
+const source = require("./source.js")
+
+describe("$apply", () => {
+    it("top level", () => {
+        const updated = update(
+            source,
+            {$apply: Object.keys}
+        )
+
+        expect(updated.sort())
+            .toEqual(["nested", "top", "topArray"])
+    })
+    it("first level", () => {
+        const updated = update(
+            source,
+            {"top.$apply": i => i ** 2}
+        )
+
+        expect(updated)
+            .toEqual({
+                ...source,
+                top: 100
+            })
+    })
+    it("nested", () => {
+        const updated = update(
+            source,
+            {"nested.value.$apply": i => i + 3}
+        )
+
+        expect(updated)
+            .toEqual({
+                top: 10,
+                nested: {
+                    value: 4,
+                    array: [4, 5, 6]
+                },
+                topArray: [1, 2, 3]
+            })
+    })
+})
