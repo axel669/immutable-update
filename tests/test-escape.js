d2cb1fe0d2e242866aa889a0f640d8a9bc5915e7
axel669 <haryuu.hanekata@gmail.com>
2019-06-05 10:47:35 -0700

    finished tests for escape sequences

--- tests/test-escape.js
+++ tests/test-escape.js
@@ -1,23 +1,68 @@
 const update = require("../index.js")
 
-const source = require("./source.js")
+const source = {
+    "top.test": 1,
+    nested: {
+        "value.thing": "wat"
+    },
+    "nested.dots": {
+        array: [],
+        "wat.woah": 3.14
+    }
+}
 
 describe("escaped", () => {
-    describe("$set", () => {
-        it("top level", () => {
-            const updated = update(
-                source,
-                {"top..test.$set": 100}
-            )
+    it("top level", () => {
+        const updated = update(
+            source,
+            {"top..test.$set": 100}
+        )
 
-            expect(updated)
-                .toEqual({
-                    "top.test": 100,
-                    ...source
-                })
-        })
+        expect(updated)
+            .toEqual({
+                ...source,
+                "top.test": 100
+            })
     })
 
-    describe("$unset", () => {
+    it("nested", () => {
+        const updated = update(
+            source,
+            {
+                "nested.value..thing.$set": "not wat",
+                "nested..dots.array.$push": 0,
+                "nested..dots.wat..woah.$apply": i => i * 2
+            }
+        )
+
+        expect(updated)
+            .toEqual({
+                "top.test": 1,
+                nested: {
+                    "value.thing": "not wat"
+                },
+                "nested.dots": {
+                    array: [0],
+                    "wat.woah": 6.28
+                }
+            })
+    })
+
+    it("$unset", () => {
+        const updated = update(
+            source,
+            {$unset: ["top.test"]}
+        )
+
+        expect(updated)
+            .toEqual({
+                nested: {
+                    "value.thing": "wat"
+                },
+                "nested.dots": {
+                    array: [],
+                    "wat.woah": 3.14
+                }
+            })
     })
 })
