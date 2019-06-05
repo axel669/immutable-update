6b7bb13288288aad7a9959b3f15ceb550b298dde
axel669 <haryuu.hanekata@gmail.com>
2019-06-05 11:15:34 -0700

    finished tests for $merge

--- tests/test-$merge.js
+++ tests/test-$merge.js
@@ -8,5 +8,35 @@ describe("$merge", () => {
             source,
             {"$merge": {a: 10}}
         )
+
+        expect(updated)
+            .toEqual({
+                ...source,
+                a: 10
+            })
+        expect(source === updated)
+            .toBe(false)
+    })
+
+    it("nested", () => {
+        const updated = update(
+            source,
+            {"nested.$merge": {hi: "there"}}
+        )
+
+        expect(updated)
+            .toEqual({
+                top: 10,
+                nested: {
+                    value: 1,
+                    array: [4, 5, 6],
+                    hi: "there"
+                },
+                topArray: [1, 2, 3]
+            })
+        expect(source.topArray === updated.topArray)
+            .toBe(true)
+        expect(source === updated)
+            .toBe(false)
     })
 })
